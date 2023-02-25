eb8fdf9e67a8f88ace45c707195e48624a0de863
Wataru.Kasahara <kasahara1019@gmail.com>
2023-02-25 14:38:13 +0900

    test: ActivatedRouteStub を用意して HeroDetailComponent のテストをする

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,16 +1,22 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRouteStub } from '../../testing/activated-route-stub';
 import { HeroDetailComponent } from './hero-detail.component';
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
   let fixture: ComponentFixture<HeroDetailComponent>;
+  let activatedRoute: ActivatedRouteStub;
 
   beforeEach(async () => {
+    activatedRoute = new ActivatedRouteStub({ id: 1 });
+
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, HttpClientTestingModule],
       declarations: [HeroDetailComponent],
+      providers: [{ provide: ActivatedRoute, useValue: activatedRoute }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeroDetailComponent);
