2e6449e89d0b27db5de6fbe9db1b441f90d960c4
Wataru.Kasahara <kasahara1019@gmail.com>
2023-02-25 14:11:35 +0900

    test: HeroDetailComponent のテストファイルを用意

---
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -0,0 +1,24 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HeroDetailComponent } from './hero-detail.component';
+
+describe('HeroDetailComponent', () => {
+  let component: HeroDetailComponent;
+  let fixture: ComponentFixture<HeroDetailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [HeroDetailComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HeroDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+});
